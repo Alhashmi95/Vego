6afe62955bbcad66c80763ae9d940db566f8b6d7
Alhashmi95 <9955ac@gmail.com>
2018-11-20 20:58:10 +0300

    google and fb

--- app/src/main/java/com/vego/vego/Activity/SignupActivity.java
+++ app/src/main/java/com/vego/vego/Activity/SignupActivity.java
@@ -5,19 +5,40 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.vego.vego.R;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
 
     private EditText confirmPassword, userPassword, userEmail;
     private Button signupBtn;
@@ -25,6 +46,20 @@ public class SignupActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
 
     private ProgressDialog progressDialog;
+    
+    private DatabaseReference databaseReference;
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    private CallbackManager mCallbackManager;
+    private LoginButton loginButton;
+
 
 
     String email, confirm, password;
@@ -39,8 +74,10 @@ public class SignupActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
 
-
+        facebookLogin();
         setupUIViews();
+
+
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -89,6 +126,232 @@ public class SignupActivity extends AppCompatActivity {
         signupBtn = (Button)findViewById(R.id.signupBtn);
         signinTxt = (TextView)findViewById(R.id.signinTxt);
     }
+
+    private void facebookLogin() {
+        // Facebook Login
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginButton mFacebookSignInButton = (LoginButton) findViewById(R.id.facebook_button);
+        mFacebookSignInButton.setReadPermissions("email");
+
+        mFacebookSignInButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                progressDialog.setMessage("جاري تسجيل الدخول ... يرجى الانتظار");
+                progressDialog.show();
+                progressDialog.setCancelable(false);
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                firebaseAuthWithFacebook(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+
+    }
+    private void firebaseAuthWithFacebook(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        final AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        } else {
+                            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if (dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()
+                                            && dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile")
+                                            .child("isAdmin").getValue().equals("false")) {
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, BottomNav.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, BottomNav.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+
+                                        //SignupActivity.this.finish();
+
+                                    } else if (dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()
+                                            && dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile")
+                                            .child("isAdmin").getValue().equals("true")) {
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, AdminActivity.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, AdminActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+                                        //SignupActivity.this.finish();
+
+                                    } else if (!dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()) {
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, UserDetails.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, UserDetails.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+                                        //SignupActivity.this.finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()
+                                            && dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile")
+                                            .child("isAdmin").getValue().equals("false") ){
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, BottomNav.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, BottomNav.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+
+                                        //SignupActivity.this.finish();
+
+                                    }else if(dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()
+                                            && dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile")
+                                            .child("isAdmin").getValue().equals("true") ){
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, AdminActivity.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, AdminActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+                                        //SignupActivity.this.finish();
+
+                                    }else if(!dataSnapshot.child("users").child(firebaseAuth.getUid()).child("Profile").exists()) {
+                                        //SignupActivity.this.startActivity(new Intent(SignupActivity.this, UserDetails.class));
+
+                                        Intent intent = new Intent(SignupActivity.this, UserDetails.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+
+                                        progressDialog.dismiss();
+                                        //SignupActivity.this.finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                    }
+                });
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed
+                Log.e(TAG, "Google Sign In failed.");
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google_button:
+                signIn();
+                break;
+            default:
+                return;
+        }
+    }
+
     private Boolean validate(){
         Boolean result = false;
 
